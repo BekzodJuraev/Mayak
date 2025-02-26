158ef7f773e22f92af7f91893ddf916a076cb40e
Nurbek Juraev <powerzver98@gmail.com>
2025-02-26 21:42:01 +0500

    added models staff,orders,partners

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Parnters(models.Model):
+    pictures=models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.pictures
+
+class Staff(models.Model):
+    photo=models.ImageField(upload_to='staff/')
+    status=models.CharField(max_length=250)
+    name=models.CharField(max_length=250)
+    description=models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    name=models.CharField(max_length=250)
+    phone=models.CharField(max_length=250)
+    email=models.EmailField()
+    message=models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
