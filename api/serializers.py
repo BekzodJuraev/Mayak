64e2d642e96bd40c88bf8db1f8254fcec4db03be
Bekzod <powerzver98@gmail.com>
2025-03-05 17:06:23 +0500

    added google sheet

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
 from .models import Staff,Parnters,Items,Orders,Basket,Basketproducts,Category
+from googleapiclient.discovery import build
+from config import BOT_TOKEN,CHANEL_SUPPORT,API_KEY_GOOGLE
+import telegram
+
+SPREADSHEET_ID = "1z7CKVIOjz30amDeUlFkPZl-mSNW_gymkASNSv-Ot10E"
+RANGE_NAME = "Лист1!B2:B"
+service = build("sheets", "v4", developerKey=API_KEY_GOOGLE)
+
+bot=telegram.Bot(BOT_TOKEN)
 
 class Staffserizzers(serializers.ModelSerializer):
     class Meta:
@@ -35,10 +44,43 @@ class BasketSer(serializers.ModelSerializer):
     def create(self, validated_data):
         products = validated_data.pop('products')
         basket = Basket.objects.create(**validated_data)
-        for item in products:
-            add_item = item.pop('items')
-            count = item.pop('count')
-            Basketproducts.objects.create(basket=basket, items=add_item, count=count)
+        Basketproducts.objects.bulk_create([
+            Basketproducts(basket=basket, items=item['items'], count=item['count'])
+            for item in products
+        ])
+
+        product_list = "\n".join([
+            f"🔹 {item['items']} (Кол-во: {item['count']})" for item in products
+        ])
+
+        message = (
+            f" Имя: {basket.name}\n"
+            f"📞 Телефон: {basket.phone}\n"
+            f"📦 Товары:\n{product_list}"
+        )
+        try:
+            # Fetch data
+            sheet = service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+            values = result.get("values", [])
+
+            # Print the values
+            if not values:
+                pass
+            else:
+                for row in values:
+                    phone = basket.phone.replace("+", "")
+                    if phone in row[0]:
+                        message += "\nЭто Резидент"
+
+
+        except Exception as e:
+            pass
+
+        bot.send_message(CHANEL_SUPPORT, text=message)
+
+
+
         return basket
 
 
--- api/signals.py
+++ api/signals.py
@@ -3,11 +3,7 @@ from django.dispatch import receiver
 import telegram
 from .models import Orders,Basket,Basketproducts
 from config import BOT_TOKEN,CHANEL_SUPPORT,API_KEY_GOOGLE
-from googleapiclient.discovery import build
-API_KEY = API_KEY_GOOGLE
-SPREADSHEET_ID = "1z7CKVIOjz30amDeUlFkPZl-mSNW_gymkASNSv-Ot10E"
-RANGE_NAME = "Лист1!B2:B"
-service = build("sheets", "v4", developerKey=API_KEY)
+
 bot=telegram.Bot(BOT_TOKEN)
 group_id=CHANEL_SUPPORT
 
@@ -17,37 +13,4 @@ def send_message(sender,instance,created,*args,**kwargs):
         bot.send_message(group_id,f'Имя:{instance.name}\nТелефон:{instance.phone}\nПочта:{instance.email}\nСообщение:{instance.message}')
 
 
-@receiver(post_save,sender=Basketproducts)
-def basket_message(sender,instance,created,*args,**kwargs):
-    if created:
-
-        basket = instance.basket
-        products = basket.products.all()
-
-
-        product_list = "\n".join([f"🔹 {item.items} (Кол-во: {item.count})" for item in products])
-
-        # Формируем итоговый текст
-        message = (
-            f" Имя: {basket.name}\n"
-            f"📞 Телефон: {basket.phone}\n"
-            f"📦 Товары:\n{product_list}"
-        )
-        try:
-            # Fetch data
-            sheet = service.spreadsheets()
-            result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
-            values = result.get("values", [])
-
-            # Print the values
-            if not values:
-                pass
-            else:
-                for row in values:
-                    if basket.phone in row[0]:
-                       message+="Это Резидент"
-
-        except Exception as e:
-            pass
-        bot.send_message(group_id,text=message)
 
