cc3fc48bba7db9e414a4e9453a0e4d1a40457e96
Nurbek Juraev <powerzver98@gmail.com>
2025-03-12 17:57:16 +0500

    added eskiz

---
+++ eskis.uz.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://notify.eskiz.uz/api/message/sms/send"
+
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDQzNzU3NDMsImlhdCI6MTc0MTc4Mzc0Mywicm9sZSI6InRlc3QiLCJzaWduIjoiYWY0MWI3Y2U0Yzg2MzY3YTJmNmIxMDI3OWEyNmRjODljNWJjYThhYjcxZjEwMjg2NTAwMGQ4M2UxYjE2Mjc4MCIsInN1YiI6IjEwMDkzIn0.tAlKryrsmqtk1BTl1d640a2LP1Lhh49uCTN8GqCrlbw"
+
+payload = {
+    'mobile_phone': '906566970',
+    'message': 'Это тест от Eskiz',
+    'from': 'Mayak',
+}
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+response = requests.post(url, headers=headers, data=payload)
+
+print(response.text)
